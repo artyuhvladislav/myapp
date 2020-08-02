165a85ad8ebe6b429f3985a9afb05675045bee65
artyuhvladislav <workto22@gmail.com>
2020-08-02 17:26:10 +0300

    Updates

---
+++ precache-manifest.e6466501a6e5d330c264c0cb1114f628.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b41d83c3a46eca9e51458bca1fe79f4",
+    "url": "/my-app/index.html"
+  },
+  {
+    "revision": "621c04d17aca9da01897",
+    "url": "/my-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "926ac0cad321f222793f",
+    "url": "/my-app/static/js/2.f7035d65.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/my-app/static/js/2.f7035d65.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "621c04d17aca9da01897",
+    "url": "/my-app/static/js/main.6ded6ba6.chunk.js"
+  },
+  {
+    "revision": "fbcaf22c94940e0dce9c",
+    "url": "/my-app/static/js/runtime-main.e7af08a9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
